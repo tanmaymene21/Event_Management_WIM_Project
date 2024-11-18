d5cd77d3f9c21674d6c43c16beda45499cbce477
TanmayMene <tanmaymene2004@gmail.com>
2024-11-18 22:32:24 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); 
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
@@ -15,8 +21,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`),
+);
