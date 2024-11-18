bad49374c86369b74b911f74985db3f203e6c464
TanmayMene <tanmaymene2004@gmail.com>
2024-11-18 22:40:05 +0530

    cors removed

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,6 @@ const eventRoutes = require('./routes/eventRoutes');
 const app = express();
 
 app.use(cors());
-app.use(
-  cors({
-    origin: `${process.env.FRONTEND_URL}`,
-    credentials: true,
-  }),
-);
 app.use(express.json());
 
 // Routes
